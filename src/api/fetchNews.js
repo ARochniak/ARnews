d4b73ccab341b3300053f7a9686236d5b1b2d0ed
ARochniak <15bblinn@gmail.com>
2020-01-16 11:48:33 +0200

    Add sort and split request

--- src/api/fetchNews.js
+++ src/api/fetchNews.js
@@ -1,6 +1,10 @@
 const fetchNews = async (category = 'world', count = 10) => {
+  const queryCategory = `category=${category}`;
+  const queryCount = `count=${count}`;
+
   const response = await fetch(
-    `https://api.cognitive.microsoft.com/bing/v7.0/news?mkt=en-us&category=${category}&count=${count}`,
+    'https://api.cognitive.microsoft.com/bing/v7.0/news?' +
+      `mkt=en-us&sortBy=Date&${queryCategory}&${queryCount}`,
     {
       headers: {
         'Ocp-Apim-Subscription-Key': '6808d519218c4eeba588ed04da827f0b'
