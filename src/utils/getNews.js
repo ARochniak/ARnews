5556c43682728f9522a36a13849d4c5836c4f15f
ARochniak <15bblinn@gmail.com>
2020-02-09 20:23:07 +0200

    Use localStorage for development

--- src/utils/getNews.js
+++ src/utils/getNews.js
@@ -1,9 +1,19 @@
 import fetchNews from '../api/fetchNews';
 
 const getNews = async (category, count) => {
-  // TODO save localStorage for 15 minutes
-  const localNews = localStorage.getItem('newsArray');
-  if (localNews) return JSON.parse(localNews);
+  // TODO only for development process save locally for 15 minutes
+  if (category === 'World') {
+    const localNews = localStorage.getItem('newsArray');
+    if (localNews) {
+      const timeSaved = localStorage.getItem('timeSaved');
+      if (timeSaved) {
+        const timePassed = new Date() - new Date(timeSaved);
+        if (timePassed < 15 * 60 * 1000) {
+          return JSON.parse(localNews);
+        }
+      }
+    }
+  }
   const response = await fetchNews(category.toLowerCase(), count);
   const newsArray = response.map((newsItem, i) => ({
     id: i,
@@ -14,7 +24,10 @@ const getNews = async (category, count) => {
       ? newsItem.image.thumbnail.contentUrl
       : newsItem.urlToImage
   }));
-  localStorage.setItem('newsArray', JSON.stringify(newsArray));
+  if (category === 'World') {
+    localStorage.setItem('newsArray', JSON.stringify(newsArray));
+    localStorage.setItem('timeSaved', new Date().toString());
+  }
   return newsArray;
 };
 
