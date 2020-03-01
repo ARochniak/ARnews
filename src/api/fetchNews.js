173fab51e4e9a8718ea0b67dd3d84b5f1f09776d
Anton Rochniak <rochniak15@gmail.com>
2020-03-01 11:36:26 +0200

    Split fetchNews

--- src/api/fetchNews.js
+++ src/api/fetchNews.js
@@ -1,27 +1,32 @@
-const fetchNews = async (category = 'world', count = 10) => {
-  let queryCategory = `category=${category}`;
-  let queryCount = `count=${count}`;
-
-  let response = await fetch(
+const fetchAzureNews = async (category, count) => {
+  const response = await fetch(
     'https://api.cognitive.microsoft.com/bing/v7.0/news?' +
-      `mkt=en-us&sortBy=Date&${queryCategory}&${queryCount}`,
+      `mkt=en-us&sortBy=Date&category=${category}&count=${count}`,
     {
       headers: {
         'Ocp-Apim-Subscription-Key': '6808d519218c4eeba588ed04da827f0b'
       }
     }
   );
-  let json = await response.json();
-  if (!json.error) return json.value;
-  queryCategory =
-    category === 'world' ? `category=general` : `category=${category}`;
-  queryCount = `pageSize=${count}`;
-  response = await fetch(
+  const json = await response.json();
+  return json.error ? false : json.value;
+};
+
+const fetchNewsApi = async (category, count) => {
+  const response = await fetch(
     'https://newsapi.org/v2/top-headlines?country=us&' +
-      `apiKey=fddc6392d1c449f2aefb2da74803024e&${queryCategory}&${queryCount}`
+      `apiKey=fddc6392d1c449f2aefb2da74803024e&category=${category}&pageSize=${count}`
   );
-  json = await response.json();
+  const json = await response.json();
   return json.articles;
+};
+
+const fetchNews = async (category = 'world', count = 10) => {
+  const azureRes = await fetchAzureNews(category, count);
+  if (azureRes) return azureRes;
+  const newsApiCategory = category === 'world' ? `general` : category;
+  const newsApiRes = await fetchNewsApi(newsApiCategory, count);
+  return newsApiRes;
 };
 
 export default fetchNews;
