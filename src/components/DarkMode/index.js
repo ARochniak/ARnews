0e3449770d351c135d28f2f59d7f8b582b064a27
Anton Rochniak <rochniak15@gmail.com>
2020-04-02 11:44:18 +0300

    Add dark-mode key handler

--- src/components/DarkMode/index.js
+++ src/components/DarkMode/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import './index.css';
-// TODO add tabindex
+
 const DarkMode = ({ darkModeToggle }) => {
+  const keyHandler = e => {
+    if (e.keyCode === 32 || e.keyCode === 13) {
+      darkModeToggle();
+    }
+  };
   return (
     <div className="dark-mode">
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label className="dark-mode__switch">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control,jsx-a11y/no-noninteractive-tabindex,jsx-a11y/no-noninteractive-element-interactions */}
+      <label className="dark-mode__switch" tabIndex="0" onKeyDown={keyHandler}>
         <input type="checkbox" onClick={darkModeToggle} />
         <span className="dark-mode__slider" />
       </label>
