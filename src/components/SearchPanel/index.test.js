d2d5286be5f09d2395a258626c18a5b165bf29bd
ARochniak <15bblinn@gmail.com>
2020-04-17 18:37:26 +0300

    Search-panel tests expansion

--- src/components/SearchPanel/index.test.js
+++ src/components/SearchPanel/index.test.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { SearchPanel } from './index';
 
 describe('SearchPanel', () => {
+  const dispatch = jest.fn();
   test('should trigger dispatch on form submit', () => {
-    const dispatch = jest.fn();
     const wrapper = mount(<SearchPanel dispatch={dispatch} />);
     wrapper.find('form').simulate('submit', {
       preventDefault: () => {}
     });
     expect(dispatch.mock.calls.length).toBe(1);
+  });
+  test('should has field with changing class "field_hide"', () => {
+    const wrapper = shallow(<SearchPanel dispatch={dispatch} />);
+    expect(wrapper.find('input').hasClass('field_hide')).toBe(true);
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find('input').hasClass('field_hide')).toBe(false);
   });
 });
